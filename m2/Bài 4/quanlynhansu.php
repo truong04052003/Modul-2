d7fd0d316d678c866b5efb36fe810dacf82a39ba
truong04052003 <tranvantruong10b2@gmail.com>
2022-10-21 15:59:39 +0700

    máy-tính-ck-sp

--- m2/Bài 4/quanlynhansu.php
+++ m2/Bài 4/quanlynhansu.php
@@ -1,20 +1,56 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php
+if ( $_SERVER['REQUEST_METHOD']== 'POST') {
+    $Ho = $_REQUEST['Ho'];
+    $Ten = $_REQUEST['Ten'];
+    $NgaySinh = $_REQUEST['NgaySinh'];
+    $DiaChi = $_REQUEST['DiaChi'];
+    $ViTriCongViec = $_REQUEST['ViTriCongViec'];
+session_start();
+class NhanVien{
+    public string $Ho;
+    public string $Ten;
+    public $NgaySinh;
+    public string $DiaChi;
+    public string $ViTriCongViec;
+    public array $mang = [];
+    public function __construct($Ho ='',$Ten='',$NgaySinh=0,$DiaChi='',$ViTriCongViec=''){
+        $this->Ho = $Ho;
+        $this->Ten = $Ten;
+        $this->NgaySinh = $NgaySinh;
+        $this->DiaChi = $DiaChi;
+        $this->ViTriCongViec = $ViTriCongViec;
+    }
+    public function getInfo(){
+        return [
+            "Ho" => $this->getHo(),
+            "Ten" => $this->getTen(),
+            "NgaySinh"=>$this->getNgaySinh(),
+            "DiaChi"=>$this->getDiaChi(),
+            "ViTriCongViec"=>$this->getViTriCongViec()
+        ];
+    }
+    public function getHo(){
+        return $this->Ho;
+    }
+    public function getTen(){
+        return $this->Ten;
+    }
+    public function getNgaySinh(){
+        return $this->NgaySinh;
+    }
+    public function getDiaChi(){
+        return $this->DiaChi;
+    }
+    public function getViTriCongViec(){
+        return $this->ViTriCongViec;
+    }
+}
+$objNhanVien =new NhanVien ($Ho,$Ten,$NgaySinh,$DiaChi,$ViTriCongViec);
+echo '<pre>';
+print_r($objNhanVien);
+echo '</pre>';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,49 +60,19 @@
     <title>Document</title>
 </head>
 <body>
-<form method="post">
-        <label for=""fname >Họ</label> <br>
-        <input type="text" id="" name="ho"><br><br>
-        <label for="">Tên</label><br>
-        <input type="text" id="" name="ten"><br><br>
-        <label for=""fname>Ngày sinh</label><br>
-        <input type="date" id="" name="ngaysinh"><br><br>
-        <label for=""fname>Địa chỉ</label><br>
-        <input type="text" id="" name="diachi"><br><br>
-        <label for="" fname >Vị trí công việc</label><br>
-        <input type="text" id="" name="vitricongviec"><br><br>
-        <input type="submit" value="Submit">
+<form action="" method="post">
+  <label for="fname">Họ</label><br>
+  <input type="text" id="" name="Ho" ><br>
+  <label for="fname">Tên</label><br>
+  <input type="text" id="" name="Ten" ><br>
+  <label for="fname">Ngày sinh</label><br>
+  <input type="date" id="" name="NgaySinh" ><br>
+  <label for="fname">Địa chỉ</label><br>
+  <input type="text" id="" name="DiaChi" ><br>
+  <label for="fname">Vị trí công việc</label><br>
+  <input type="text" id="" name="ViTriCongViec" ><br>
+  <input type="submit" value="Submit">
+</form> 
 
-    </form>
-    <table border="1">
-        <caption>
-            <h2>Danh sách khách hàng</h2>
-        </caption>
-
-        <thead>
-            <tr>
-                <th>Họ</th>
-                <th>Tên</th>
-                <th>Ngày sinh</th>
-                <th>Địa chỉ</th>
-                <th>Vị trí công việc</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($customerList as $key => $value) : ?>
-                <tr>
-                    <td><?php echo $key ?></td>
-                    <td><?php echo $value['ho'] ?></td>
-                    <td><?php echo $value['ten'] ?></td>
-                    <td><?php echo $value['ngaysinh'] ?></td>
-                    <td><?php echo $value['diachi'] ?></td>
-                    <td><?php echo $value['vitricongviec'] ?></td>
-                
-                </tr>
-            <?php endforeach; ?>
-
-        </tbody>
-
-    </table>
 </body>
 </html>
