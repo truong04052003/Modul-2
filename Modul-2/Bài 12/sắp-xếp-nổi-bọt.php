ef0c4b006a8776876c20d9911e8ba5a284ce3823
truong04052003 <tranvantruong10b2@gmail.com>
2022-11-03 11:40:29 +0700

    baitapsapxepnoibot

--- Modul-2/Bài 12/sắp-xếp-nổi-bọt.php
+++ Modul-2/Bài 12/sắp-xếp-nổi-bọt.php
@@ -4,9 +4,9 @@ $A = [8 , 6 , 34 , 22 , 40 , 5 , 11 , 23 , 44 , 18 ];
   //  0   1    2    3    4    5   6   7     8    9
 
 for ($i = 0; $i < count($A); $i++){
-    for ($k = $n-1 ; $k > 0; $k--){
+    for ($k = count($A)-1 ; $k > 0; $k--){
         //nếu phần tử hiện tại nhỏ hơn phần tử đứng sau thì hoán đổi
-          // 8   <   44
+          // 18   <   44
         if($A[$k] < $A[$k-1]){
             //hoán đổi
             $temp = $A[$k-1];  //44
